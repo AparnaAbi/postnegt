a756176b615b63f1b7fc4594ca25db130112030c
AparnaAbi <aparna8696@gmail.com>
2016-07-08 10:15:20 +0530

    finish

---
+++ postnegt.c
@@ -0,0 +1,15 @@
+# postnegt
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+printf("enter the number:");
+scanf("%d",&n);
+if(n<0)
+printf("the number is negative");
+else if(n>0)
+printf("the number is positive");
+else
+printf("the number is zero");
+}
